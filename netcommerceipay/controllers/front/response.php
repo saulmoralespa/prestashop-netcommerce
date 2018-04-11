66aedd0fe79d628935cfb0c2774180109d759fac
saulmoralespa <moralespachecopablo@gmail.com>
2018-04-11 12:43:42 -0500

    fixed response controller

--- netcommerceipay/controllers/front/response.php
+++ netcommerceipay/controllers/front/response.php
@@ -5,53 +5,43 @@ class NetcommerceIpayResponseModuleFrontController extends ModuleFrontController
     {
         parent::initContent();
 
-        $this->context = Context::getContext();
-
-        if (!isset($_POST['RespVal']))
+        if (!isset($_POST))
             die('No params received');
-
-	$txtMerchNum = $_POST['txtMerchNum'];
-	$txtIndex = $_POST['txtIndex'];
-	$txtAmount = $_POST['txtAmount'];
-	$txtCurrency = $_POST['txtCurrency'];
-	$txtNumAut = $_POST['txtNumAut'];
-	$RespVal = $_POST['RespVal'];
-	$RespMsg = $_POST['RespMsg'];
-	$NCSignature = $_POST['signature'];
-
-	$netcommerceipay = new NetcommerceIpay;
-	$sha_key = $netcommerceipay->sha_key;
-
+        $txtMerchNum = $_POST['txtMerchNum'];
+        $txtIndex = $_POST['txtIndex'];
+        $txtAmount = $_POST['txtAmount'];
+        $txtCurrency = $_POST['txtCurrency'];
+        $txtNumAut = $_POST['txtNumAut'];
+        $RespVal = $_POST['RespVal'];
+        $RespMsg = $_POST['RespMsg'];
+        $NCSignature = $_POST['signature'];
+        $netcommerceipay = new NetcommerceIpay;
+        $sha_key = $netcommerceipay->sha_key;
         $delimit = explode('NI', $txtIndex);
         $order_id = $delimit[0];
-
-	$sha256_value = $txtMerchNum.$txtIndex.$txtAmount.$txtCurrency.$txtNumAut.$RespVal.$RespMsg.$sha_key;
-	$signature = hash('sha256', $sha256_value);
-
-	$message = "";
-
-	if($NCSignature == $signature){
-        switch ($RespVal) {
-            case '1':
-                $netcommerceipay->PaymentStatus('APPROVED', $order_id);
-                $message = "Your Transaction was successful" . PHP_EOL . "Order ID: " . $txtIndex . PHP_EOL . "Authorization no.: ".$txtNumAut  . PHP_EOL . "Amount: ".$txtAmount  . PHP_EOL;
-                break;
-            case '0':
-                $netcommerceipay->PaymentStatus('DECLINED',$order_id);
-                $message = "Unfortunately your transaction was refused" . PHP_EOL . "Order ID: " . $txtIndex  . PHP_EOL . "Amount: ".$txtAmount  . PHP_EOL;
-                break;
+        $sha256_value = $txtMerchNum.$txtIndex.$txtAmount.$txtCurrency.$txtNumAut.$RespVal.$RespMsg.$sha_key;
+        $signature = hash('sha256', $sha256_value);
+        $message = "";
+        if($NCSignature == $signature){
+            switch ($RespVal) {
+                case '1':
+                    $netcommerceipay->PaymentStatus('APPROVED', $order_id);
+                    $message = "Your Transaction was successful" . PHP_EOL . "Order ID: " . $txtIndex . PHP_EOL . "Authorization no.: ".$txtNumAut  . PHP_EOL . "Amount: ".$txtAmount  . PHP_EOL;
+                    break;
+                case '0':
+                    $netcommerceipay->PaymentStatus('DECLINED',$order_id);
+                    $message = "Unfortunately your transaction was refused" . PHP_EOL . "Order ID: " . $txtIndex  . PHP_EOL . "Amount: ".$txtAmount  . PHP_EOL;
+                    break;
+            }
+        }else{
+            $message = "Security violation was detected!" . PHP_EOL . "Order ID: " . $txtIndex . PHP_EOL;
         }
-    }else{
-        $message = "Security violation was detected!" . PHP_EOL . "Order ID: " . $txtIndex . PHP_EOL;
-    }
-
         Context::getContext()->smarty->assign(
             array(
                 'message' => $message
             )
         );
-
-        $this->setTemplate('response.tpl');
+        $this->setTemplate('module:netcommerceipay/views/templates/front/response.tpl');
     }
 }
 ?>
--- netcommerceipay/views/templates/front/response.tpl
+++ netcommerceipay/views/templates/front/response.tpl
@@ -1,1 +1,8 @@
-<h1>{$message|escape:'htmlall':'UTF-8'}</h1>
+{extends file=$layout}
+{block name='content'}
+    <section id="content">
+        <div class="row">
+            <h3>{$message|escape:'htmlall':'UTF-8'}</h3>
+        </div>
+    </section>
+{/block}
